890e3e7fcbeee040e089cb7161c4b98b4cca4dba
Nicholas <nicholas.zhaoyu@gmail.com>
2015-04-07 10:33:40 -0700

    Add test for oauth callback url.

--- test/identity/auth.js
+++ test/identity/auth.js
@@ -3,13 +3,14 @@ var passport = require('koa-passport');
 var assert = require('assert');
 
 describe('auth', function() {
-  it("Return url with out port when it's 80", function() {
-    var router = {
+  var router = {
       cache: {},
       get: function(path, handler) {
         this.cache[path] = handler;
       }
     };
+
+  it("Return url with out port when it's 80", function() {
     var config = { 
       host: 'thepicsquare.com', 
       port: 80,
@@ -24,6 +25,27 @@ describe('auth', function() {
         }
       }};
     auth(router, config); 
-    assert.equal('http://thepicsquare.com/auth/facebook/callback', passport._strategies.facebook._callbackURL);
+    assert.equal('http://thepicsquare.com/auth/facebook/callback', 
+        passport._strategies.facebook._callbackURL);
+  });
+
+  it("Return url with port when it's not 80", function() {
+    var config = { 
+      host: 'thepicsquare.com', 
+      port: 8080,
+      authConfig: {
+        twitter: {
+          consumerKey: 'consumerKey',
+          consumerSecret: 'consumerSecret'
+        },
+        facebook: {
+          clientID: 'clientId',
+          clientSecret: 'clientSecret'
+        }
+      }};
+    auth(router, config); 
+    assert.equal('http://thepicsquare.com:8080/auth/facebook/callback', 
+        passport._strategies.facebook._callbackURL);
   })
+
 });
