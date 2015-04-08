accc994b4b07b82a36d0fda612033008b1467e65
Nicholas <nicholas.zhaoyu@gmail.com>
2015-04-07 19:42:18 -0700

    Add missing files.

---
+++ test/recent.js
@@ -0,0 +1,9 @@
+var auth = require('../../lib/recent');
+var passport = require('koa-passport');
+var assert = require('assert');
+
+describe('recent', function() {
+  it('Should record recent uploads.', function() {
+  });
+});
+
