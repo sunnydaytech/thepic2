2ad9c42765553453bd7202b25c9c088ae8562aea
Nicholas <nicholas.zhaoyu@gmail.com>
2015-04-11 13:55:42 -0700

    Check argv for env.

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var logger = require('winston');
 
-var config_suffix = process.env.ENV == 'prod' ? 'prod' : 'dev';
+var config_suffix = process.argv[2] == 'prod' ? 'prod' : 'dev';
 logger.info('loading config with ' + config_suffix + ' suffix.');
+logger.info(process.argv);
 
 var CONFIG = require('./lib/config/config_' + config_suffix);
 var app = require('./lib/app');
