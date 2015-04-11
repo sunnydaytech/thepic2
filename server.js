381045a42ad60c76ddda24489e2ae5804e1f4651
Nicholas <nicholas.zhaoyu@gmail.com>
2015-04-11 15:30:02 -0700

    Add header, show pic in recent

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var logger = require('winston');
 
 var config_suffix = process.env.ENV == 'prod' ? 'prod' : 'dev';
 logger.info('loading config with ' + config_suffix + ' suffix.');
+logger.info(process.argv);
 
 var CONFIG = require('./lib/config/config_' + config_suffix);
 var app = require('./lib/app');
