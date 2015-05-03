26ae3aa1a510996b25a7ac71e52e8eece0710e72
Nicholas <nicholas.zhaoyu@gmail.com>
2015-05-02 17:41:18 -0700

    Add a lot features

--- server.js
+++ server.js
@@ -1,10 +1,4 @@
-var logger = require('winston');
-
-var config_suffix = process.argv[2] == 'prod' ? 'prod' : 'dev';
-logger.info('loading config with ' + config_suffix + ' suffix.');
-logger.info(process.argv);
-
-var CONFIG = require('./lib/config/config_' + config_suffix);
+var CONFIG = require('./lib/config/config').CONFIG;
 var app = require('./lib/app');
 
 app.start(CONFIG);
