8079accfe0061f99294b4f132cdf3df2ffbbd600
Nicholas <nicholas.zhaoyu@gmail.com>
2016-02-14 05:13:41 +0000

    Update

--- init_env.sh
+++ init_env.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 curl https://raw.githubusercontent.com/creationix/nvm/v0.16.1/install.sh | sh
-nvm install v5.5.0 #Keep this updated
-nvm use v5.5.0
+source ~/.bashrc
+#Keep this updated
+nvm install v5.6.0 
+nvm use v5.6.0
 
 sudo apt-get install npm -y
 npm install
 sudo npm install -g grunt-cli
-sudo nom install -g bower
+sudo npm install -g bower
 bower install
